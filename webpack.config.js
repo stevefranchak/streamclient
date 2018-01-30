bb76b284ae18a5217ca174a3c3abbfaa78f48b6f
Steve Franchak <stevefranchak@gmail.com>
2018-01-29 23:29:31 -0500

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+     module: {
+         loaders: [
+             {
+                 test: /\.js$/,
+                 loader: 'babel-loader',
+                 query: {
+                     presets: ['env']
+                 }
+             }
+         ]
+     },
+     plugins: [
+         new webpack.optimize.UglifyJsPlugin()
+     ],
+     stats: {
+         colors: true
+     },
+     devtool: 'source-map',
+     watch: true
+};
